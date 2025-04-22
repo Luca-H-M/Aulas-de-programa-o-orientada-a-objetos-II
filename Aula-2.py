2ea83517462e8760d4b8f8ac9236ca0a6529644f
Luca-H-M <h.luca@escolar.ifrn.edu.br>
2025-04-22 11:17:25 -0300

    Update Aula-2.py
    apagao 2

--- Aula-2.py
+++ Aula-2.py
@@ -38,6 +38,12 @@ B = float(input())
 MEDIA = (A*3.5 + B*7.5)/11
 print(f'MEDIA = {MEDIA:.5f}')
 
+# 1011
+
+R = int(input())
+VOLUME = 4/3.0 * 3.14159 * R**3
+print(f'VOLUME = {VOLUME:.3f}')
+
 # 2416
 
 text = input()
