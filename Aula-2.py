22032fbd5b35ff6a7c20875ba944a19c94ceb2c7
Luca-H-M <h.luca@escolar.ifrn.edu.br>
2025-04-24 09:47:06 -0300

    Update Aula-2.py
    1050 & 2424

--- Aula-2.py
+++ Aula-2.py
@@ -144,3 +144,34 @@ if idwin == 44:
     print("sanguessuga")
 if idwin == 24:
     print("minhoca")
+
+# 1050
+
+x = int(input())
+if x == 61:
+    print("Brasilia")
+elif x == 71:
+    print("Salvador")
+elif x == 11:
+    print("Sao Paulo")
+elif x == 21:
+    print("Rio de Janeiro")
+elif x == 32:
+    print("Juiz de Fora")
+elif x == 19:
+    print("Campinas")
+elif x == 27:
+    print("Vitoria")
+elif x == 31:
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
+
+# 2424
+
+XeY = input()
+X, Y = int(XeY.split()[0]), int(XeY.split()[1])
+if 0 <= X <= 432 and 0 <= Y <= 468:
+    print("dentro")
+else:
+    print("fora")
