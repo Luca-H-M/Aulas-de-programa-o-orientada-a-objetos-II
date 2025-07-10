31bbcf23017a7ffc297053ec85ce3ca614ae12df
Luca-H-M <h.luca@escolar.ifrn.edu.br>
2025-07-10 13:22:08 +0000

    sla

--- Lista-5.py
+++ Lista-5.py
@@ -81,3 +81,86 @@ class PacienteUI:
             print(paciente.get_CPF)
             paciente.Idade()
             pacienteUI.menu()
+
+#2. Um Boleto ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Boleto:
+    def __init__(self):
+        self.__varstr = "não definido"
+        self.__varnum = "não definido"
+        self.__var4 = "00-00-0000"
+
+    def set_varstr(self, de):
+        try:
+            float(de)
+            print("Nome invalido")
+        except:
+            self.__nome = de
+     def set_varnum(self, de):
+        try:
+            float(de)
+            str(de)
+            self.__varnum = de
+        except:
+            print("varnum invalido")
+
+    def set_telefone(self, de):
+        try:
+            float(de)
+            str(de)
+            self.__telefone = de
+        except:
+            print("telefone invalido")
+
+    def set_nascimento(self, de):
+
+        self.__nascimento = de
+
+    def get_nascimento(self):
+        return self.__nascimento
+    def get_nome(self):
+        return self.__nome
+    def get_telefone(self):
+        return self.__telefone
+    def get_CPF(self):
+        return self.__CPF
+    
+    def Idade(self):
+        x = 24307.0
+        z = 0
+        y = 0
+        while y < len[self.__nascimento]
+            if y > 3 and y != 5
+                w = self.__nascimento[y]
+                if y > 5:
+                    w *= 12
+                z += w
+            y += 1
+        x -= z
+        y = 12%x 
+        y *= 12
+        x /= 12
+        print(f'Idade do paciente: {x} anos e {y} meses')
+
+    de ToString(self):
+    print(f'Nome: {get_nome} CPF: {get_CPF} nascimento: {get_nascimento} telefone: {get_telefone}')
+
+class PacienteUI:
+    @staticmethod
+    def menu(self):
+        self.escolha = input("digite: 1 – Calcular, 2 – Fim")
+    
+    @staticmethod
+    def main(self):
+        if self.escholha == 1:
+            paciente = Paciente
+            paciente.set_nascimento()
+            paciente.set_nome()
+            paciente.set_telefone()
+            paciente.set_CPF()
+            print(paciente.get_nascimento)
+            print(paciente.get_telefone)
+            print(paciente.get_nome)
+            print(paciente.get_CPF)
+            paciente.Idade()
+            pacienteUI.menu()
