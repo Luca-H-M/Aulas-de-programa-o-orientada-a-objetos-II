64a205c491e721c0dfb746d06003f19f0fb4fb6a
Luca-H-M <h.luca@escolar.ifrn.edu.br>
2025-09-25 13:14:40 +0000

    eu num gueeeerooooooo

---
+++ Semestre-2/Listas-09-10/index.py
@@ -0,0 +1,17 @@
+from templates.manterclienteUI import ManterClienteUI
+from templates.manterservicoUI import ManterServicoUI
+from templates.manterhorariosUI import ManterHorarioUI
+import streamlit as st
+
+class IndexUI:
+    def menu_admin():
+        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes","Cadastro de Serviços", "Cadastro de Horários"])
+        if op == "Cadastro de Clientes": ManterClienteUI.main()
+        if op == "Cadastro de Serviços": ManterServicoUI.main()
+        if op == "Cadastro de Horários": ManterHorarioUI.main()
+    def sidebar():
+        IndexUI.menu_admin()
+    def main():
+        IndexUI.sidebar()
+
+        IndexUI.main()
--- Semestre-2/Listas-09-10/view.py
+++ Semestre-2/Listas-09-10/view.py
@@ -1,5 +1,7 @@
 from models.cliente import Cliente, ClienteDAO
 from models.serviço import Servico, ServicoDAO
+from models.horario import Horario, HorarioDAO
+
 class View:
     def cliente_listar():
         return ClienteDAO.listar()
@@ -24,3 +26,25 @@ class View:
     def servico_excluir(id):
         servico = Servico(id, "", "", "")
         ServicoDAO.excluir(servico)
+
+
+    def horario_inserir(data, confirmado, id_cliente, id_servico):
+        c = Horario(0, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.inserir(c)
+
+    def horario_listar():
+        return HorarioDAO.listar()
+    
+    def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+        c = Horario(id, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO.atualizar(c)
+
+    def horario_excluir(id):
+        c = Horario(id, None)
+        HorarioDAO.excluir(c)
